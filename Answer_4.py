4db60754ef75eceec87050c0e773e66168b48978
tim-bokov <tim.efsky@gmail.com>
2020-08-12 23:58:09 +0300

    Update Answer_4.py

--- Answer_4.py
+++ Answer_4.py
@@ -1,3 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# setting URLs for Apple, Google and Facebook
+
+url_apple = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=AAPL&apikey=F6FR4E0EVKTSNPYQ&datatype=csv"
+url_google = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=GOOG&apikey=F6FR4E0EVKTSNPYQ&datatype=csv"
+url_facebook = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=FB&apikey=F6FR4E0EVKTSNPYQ&datatype=csv"
+
+# getting CSV results
+
+a_apple = pd.read_csv(url_apple)
+a_google = pd.read_csv(url_google)
+a_facebook = pd.read_csv(url_facebook)
+
+# transforming dataframes into numpy arrays with close price (10 years of 365 days - 52 weekends - 8 public holidays)
+
+arr_apple = (a_apple.to_numpy())[0:10*(365-52*2-8),4]
+arr_google = (a_google.to_numpy())[0:10*(365-52*2-8),4]
+arr_facebook = (a_facebook.to_numpy())[0:10*(365-52*2-8),4]
+
+N_apple = arr_apple.size
+N_google = arr_google.size
+N_facebook = arr_facebook.size
 
 PNL_apple = np.multiply(f_apple, r_apple)
 PNL_google = np.multiply(f_google, r_google)
