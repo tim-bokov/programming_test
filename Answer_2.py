8b4b871fc3e1d1bab0e1f196bc758adf722673b0
tim-bokov <tim.efsky@gmail.com>
2020-08-12 23:06:37 +0300

    Update Answer_2.py

--- Answer_2.py
+++ Answer_2.py
@@ -1,1 +1,20 @@
-
+from statsmodels.tsa.stattools import adfuller
+result_app = adfuller(r_apple)
+print('ADF Statistic: %f' % result_app[0])
+print('p-value: %f' % result_app[1])
+for key, value in result_app[4].items():
+    print('\t%s: %.3f' % (key, value))
+    
+from statsmodels.tsa.stattools import adfuller
+result_google = adfuller(r_google)
+print('ADF Statistic: %f' % result_google[0])
+print('p-value: %f' % result_google[1])
+for key, value in result_google[4].items():
+    print('\t%s: %.3f' % (key, value))
+    
+from statsmodels.tsa.stattools import adfuller
+result_facebook = adfuller(r_facebook)
+print('ADF Statistic: %f' % result_facebook[0])
+print('p-value: %f' % result_facebook[1])
+for key, value in result_facebook[4].items():
+    print('\t%s: %.3f' % (key, value))
